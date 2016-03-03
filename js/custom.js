db1b227742ddc69331bfad2110d3d41a36f39e5f
MoyArt <moyart@moyart.com.mx>
2016-03-03 17:38:06 -0600

    Se agregaron las animaciones de los elementos

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,63 @@
-jQuery(function(){
-	jQuery(window).scroll(function(){
-		var offsetX = $('html').offset().top;
-		console.log(offsetX);
-		if(offsetX < 0){
-			jQuery('#logoArea > h1').fadeOut();
-		}else{
-			jQuery('#logoArea > h1').fadeIn();
-		}
-	});
-});
+/*************************** ANIMACION DE MENU***********************************************/
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 500);
+          return false;
+        }
+      }
+    });
+  });
+
+/***************************** Animaciones de elementos ******************************************/
+var $donCroc = $('.donCroc');
+var $aDerechaIn = $('.aDerechaIn');
+var $aTopIn = $('.aTopIn');
+var $aIzqIn = $('.aIzqIn');
+
+
+
+$donCroc.waypoint(function(direction){
+  if (direction == 'down'){
+    $donCroc.addClass("js-donCroc-animado");
+  }else{
+    $donCroc.removeClass("js-donCroc-animado");
+  }
+}, {offset:95} );
+
+
+$aDerechaIn.waypoint(function(direction){
+  if (direction == 'down'){
+    $aDerechaIn.addClass("js-aDerechaIn-animado");
+  }else{
+    $aDerechaIn.removeClass("js-aDerechaIn-animado");
+  }
+}, {offset:450} );
+
+$aDerechaIn.waypoint(function(direction){
+  if (direction == 'down'){
+    $aTopIn.addClass("js-aTopIn-animado");
+  }else{
+    $aTopIn.removeClass("js-aTopIn-animado");
+  }
+}, {offset:450} );
+
+$aDerechaIn.waypoint(function(direction){
+  if (direction == 'down'){
+    $aIzqIn.addClass("js-aIzqIn-animado");
+  }else{
+    $aIzqIn.removeClass("js-aIzqIn-animado");
+  }
+}, {offset:450} );
+
+
+
+
+
+
+
